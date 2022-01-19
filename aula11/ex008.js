dba59f0846b9b20a3558d137ccfa93619aa54160
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-01-19 19:03:58 -0300

    comit

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+//console.log('O console funcionou corretamente!')
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel > 60) {//Condição simles!
+    console.log(`Voce ultrapassou a velocidade permitida. MULTADO`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Voce esta vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Voce é brasileiro!')
+}else{
+    console.log('Voce é estrangeiro!')
+}
