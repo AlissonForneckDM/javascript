61abd6f46063bd064334b3e93f9ec9d44898bc06
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-01-24 11:18:36 -0300

    fim aula 13

--- aula13/modelo/script.js
+++ aula13/modelo/script.js
@@ -9,6 +9,34 @@ function verificar(){
         window.alert('Verifique os dados e tente novamente')
     }else{
         var sexo = document.getElementsByName('radsex')
-        window.alert(`${soma} ${sexo}`)
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (sexo[0].checked){
+            genero = 'Homem'
+            if(soma < 10){
+                img.setAttribute('src' , 'imagens/crianca-menino.jpg')
+            }else if (soma < 21){
+                img.setAttribute('src', 'imagens/homem-j.jpg')
+            }else if (soma < 50){
+                img.setAttribute('src', 'imagens/homem-a.jpg')
+            }else{
+                img.setAttribute('src', 'imagens/homem-i.jpg')
+            }
+        }else{
+            genero = 'Mulher'
+            if(soma < 10){
+                img.setAttribute('src', 'imagens/crianca-menina.jpg')
+            }else if (soma < 21){
+                img.setAttribute('src', 'imagens/mulher-j.jpg')
+            }else if (soma < 50){
+                img.setAttribute('src', 'imagens/mulher-a.jpg')
+            }else{
+                img.setAttribute('src', 'imagens/mulher-i.jpg')
+            }
+        }
+        conteudo.style.textAlign = 'center'
+        conteudo.innerHTML = `Voce é ${genero} com idade ${soma}<br>`
+        conteudo.appendChild(img)
+        
     }
 }
