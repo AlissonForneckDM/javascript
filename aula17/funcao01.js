69455d2b634f51adc08f6e396b8357a8c70be2db
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-01-25 11:12:17 -0300

    funcoes

---
+++ aula17/funcao01.js
@@ -0,0 +1,12 @@
+function parimp(n){
+    if(n % 2 == 0){
+        return 'par'
+    }else{
+        return 'impar'
+    }
+}
+/*
+var res = parimp(10)
+console.log(res)
+*/
+console.log(parimp(1043))
---
+++ aula17/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0){
+    return n1 + n2
+}
+console.log(soma(4,5))
+//para tratar erros caso o usuario não informe o valor, podemos ja deixar um valor definido na função
