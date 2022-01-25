de4834c3450b91057c78b2c5aea20067d3024fd4
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-01-24 21:42:02 -0300

    ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,42 +1,19 @@
-
 function verificar(){
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.querySelector('input#idade')
-    var anonasc = Number(fano.value)
-    var soma = ano - anonasc
-    if(fano.value.length == 0 || fano.value > ano){
-        window.alert('Verifique os dados e tente novamente')
+    var tabu = document.querySelector('input#num')
+    var tab = document.querySelector('select#tabuada')
+    var num = Number(tabu.value)
+    if(num == 0){
+        window.alert('Digite um numero valido')
     }else{
-        var sexo = document.getElementsByName('radsex')
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (sexo[0].checked){
-            genero = 'Homem'
-            if(soma < 10){
-                img.setAttribute('src' , 'imagens/crianca-menino.jpg')
-            }else if (soma < 21){
-                img.setAttribute('src', 'imagens/homem-j.jpg')
-            }else if (soma < 50){
-                img.setAttribute('src', 'imagens/homem-a.jpg')
-            }else{
-                img.setAttribute('src', 'imagens/homem-i.jpg')
-            }
-        }else{
-            genero = 'Mulher'
-            if(soma < 10){
-                img.setAttribute('src', 'imagens/crianca-menina.jpg')
-            }else if (soma < 21){
-                img.setAttribute('src', 'imagens/mulher-j.jpg')
-            }else if (soma < 50){
-                img.setAttribute('src', 'imagens/mulher-a.jpg')
-            }else{
-                img.setAttribute('src', 'imagens/mulher-i.jpg')
-            }
+        tab.innerHTML=''
+        for (var c=1 ; c <= 10 ; c++){
+            var resul = c * num
+            var item = document.createElement('option')
+            item.text =`${num} X ${c} = ${resul}`
+            tab.value =`tab${c}`
+            tab.appendChild(item)
+            //conteudo.innerHTML += `${c} x ${num} = ${resul}<br>`
+            //conteudo.innerHTML += `${resul}`
         }
-        conteudo.style.textAlign = 'center'
-        conteudo.innerHTML = `Voce é ${genero} com idade ${soma}<br>`
-        conteudo.appendChild(img)
-        
     }
 }
