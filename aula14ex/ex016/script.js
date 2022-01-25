b084c344b68d7a363ef6c79fb6b78ef2bc6b5367
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-01-24 21:04:59 -0300

    fim ex16

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,18 +2,30 @@ function verificar(){
     var inicio = document.querySelector('input#inicio')
     var fim = document.querySelector('input#fim')
     var passo = document.querySelector('input#passo')
+    res = document.querySelector('div#conteudo')
     var i = Number(inicio.value)
     var f = Number(fim.value)
     var p = Number(passo.value)
-    conteudo.innerHTML = ` ${i} ${f} ${p}`
-    if(i >= f || p > f ){
+    //conteudo.innerHTML = ` ${i} ${f} ${p}`
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
         window.alert('Digite valores validos')
-    }else if (f==0 || p ==0){
-        //for(var c=i; c <= f ; c+p){
-            //conteudo.innerHTML += `${c}`
-            conteudo.innerHTML ='to aqui 1'
-        }else{
-            conteudo.innerHTML ='to aqui'
+        res.innerHTML = 'Não foi possivel contar'
+        //conteudo.innerHTML += `${c}`
+    }else{
+        if(p <= 0){
+            window.alert('Passo invalido, vou considerar o PASSO 1')
+            p = 1
         }
+        if (i < f){
+        res.innerHTML = 'Contando...<br>'
+        for (var c=i; c<=f; c+=p){
+            conteudo.innerHTML += ` ${c} \u{1F449}`
+        }
+        }else{
+            for (var c = i; c >=f; c -= p){
+                conteudo.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
