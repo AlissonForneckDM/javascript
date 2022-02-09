289665403ce79ccf7838f316860d3af128bd2d7b
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-09 20:30:10 -0300

    relembrando ifs

--- Alura/JS POO/aula2.js
+++ Alura/JS POO/aula2.js
@@ -20,4 +20,12 @@ const contaRicardo = new ContaCorrente()
 contaRicardo.saldo = 0
 contaRicardo.agencia = 1001
 
-console.log()
+console.log(contaRicardo.saldo)
+contaRicardo.saldo = 100
+console.log(contaRicardo.saldo)
+let valorSacado = 50
+if(valorSacado > contaRicardo.saldo){
+    console.log('Voce não pode continuar com essa operação')
+}else{
+    contaRicardo.saldo -= valorSacado
+}console.log(contaRicardo.saldo)
