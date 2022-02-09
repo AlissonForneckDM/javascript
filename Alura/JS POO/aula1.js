980fe9405295793231a9c38a35fe63436cd2b307
Alisson Forneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-09 20:24:59 -0300

    aprendendo mais sobre classes no js

---
+++ Alura/JS POO/aula1.js
@@ -0,0 +1,20 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia
+    saldo;
+}
+
+const cliente1 = new Cliente()
+cliente1.nome = 'Ricardo'
+cliente1.cpf  = 11122233388
+cliente1.agencia = 1001
+cliente1.saldo = 0
+console.log(cliente1)
+
+const cliente2 = new Cliente()
+cliente2.nome = 'Alice'
+cliente2.cpf = 22233344488
+cliente2.agencia= 1001
+cliente2.saldo= 0
+console.log(cliente2)
---
+++ Alura/JS POO/aula2.js
@@ -0,0 +1,23 @@
+//exercitando e expandindo conhecimentos sobre classes
+
+class Cliente {
+    nome;
+    cpf;
+}
+class ContaCorrente{
+    agencia;
+    saldo;
+}
+const cliente1 = new Cliente()
+cliente1.nome = 'Roger'
+cliente1.cpf = 66677788899
+
+const cliente2 = new Cliente()
+cliente2.nome = 'Maria'
+cliente2.cpf = 11122233344
+
+const contaRicardo = new ContaCorrente()
+contaRicardo.saldo = 0
+contaRicardo.agencia = 1001
+
+console.log()
