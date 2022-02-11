21bf7f769be8771efe7999df4fc79073e9a228e0
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-11 11:56:36 -0300

    criei o construtor da classe poupanca

---
+++ Alura/JS-herancas-obj/ContaPoupanca.js
@@ -0,0 +1,7 @@
+export class ContaPoupanca{
+    constructor(saldoInicial,cliente,agencia){
+        this.saldoInicial = saldoInicial
+        this._cliente = cliente
+        this._agencia = agencia
+    }
+}
