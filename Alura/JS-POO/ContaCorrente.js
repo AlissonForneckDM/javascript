0908718503c13547aa3a04bb568acaf96d7c2db4
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-10 21:44:59 -0300

    trabalhand com construtores e getters e setters

--- Alura/JS-POO/ContaCorrente.js
+++ Alura/JS-POO/ContaCorrente.js
@@ -1,11 +1,32 @@
+import { Cliente } from "./Cliente.js";
+// para deixar um metodo privado não oficial #saldo
+// https://github.com/tc39/proposal-class-fields#private-fields
+// por convenção da sociedade atualmente usamos o 
+    
 export class ContaCorrente{
     agencia;
-    cliente;
-    
-    // para deixar um metodo privado não oficial #saldo
-    // https://github.com/tc39/proposal-class-fields#private-fields
-    // por convenção da sociedade atualmente usamos o _saldo
+    _cliente;
+
+    set cliente(nomeCliente){
+        if(nomeCliente instanceof(Cliente)){
+            this._cliente = nomeCliente
+        }
+    }
+
+    get cliente(){
+        return this._cliente
+    }
+
     _saldo = 0;
+
+    get saldo(){
+        return this._saldo
+    }
+
+    constructor(cliente, agencia){
+        this.cliente = cliente
+        this.agencia = agencia
+    }
 
     saque(valor){
         if (this._saldo >= valor){
@@ -17,5 +38,10 @@ export class ContaCorrente{
     deposito(valor){
         if(valor <= 0) return;
         this._saldo += valor
+    }
+
+    transferir(valor, conta){
+        const valorTransferido = this.saque(valor)
+        conta.deposito(valorTransferido)
     }
 }
--- Alura/JS-POO/aula2.js
+++ Alura/JS-POO/aula2.js
@@ -1,24 +1,19 @@
 //exercitando e expandindo conhecimentos sobre classes
 import { Cliente } from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
-const cliente1 = new Cliente()
-cliente1.nome = 'Roger'
-cliente1.cpf = 66677788899
 
-const contaRoger = new ContaCorrente()
-contaRoger.agencia = 1001
-contaRoger.cliente = cliente1
 
-contaRoger.deposito(100)
-const valorSacado = contaRoger.saque(50)
+const cliente1 = new Cliente('Roger',66677788899)
+
+const contaRoger = new ContaCorrente(1001, cliente1)
+
+contaRoger.deposito(500)
+
+const cliente2 = new Cliente('Maria', 11122233344)
+
+const contaMaria = new ContaCorrente(102, cliente2);
 console.log(contaRoger)
-console.log(valorSacado)
+//contaMaria.cliente = new Cliente()--------forma diferente de instanciar uma classe dentro de uma variavel de classe
+//contaMaria.cliente.nome= "Alice"
+//contaMaria.cliente.cpf = 11122233344
 
-const cliente2 = new Cliente()
-cliente2.nome = 'Maria'
-cliente2.cpf = 11122233344
-
-const contaMaria = new ContaCorrente();
-contaMaria.agencia = 102
-contaMaria.cliente = cliente2
-console.log(contaMaria)
