1a38677bcc6c5cd8acda8c189e66eb263e48eb74
alissonforneck <33720226+alissonforneck@users.noreply.github.com>
2022-02-13 18:48:52 -0300

    Create Conta.js

---
+++ Alura/JS-herancas-obj/Conta.js
@@ -0,0 +1,22 @@
+export class Conta{
+    constructor(saldoInicial,cliente,agencia){
+        this.saldoInicial = saldoInicial
+        this._cliente = cliente
+        this._agencia = agencia
+    }
+    saque(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor
+        }return valor
+    }
+
+    deposito(valor){
+        if(valor <= 0 ) return
+            this._saldo = valor
+    }
+
+    transferir(valor, conta){
+        const valorTransferido = this.saque(valor)
+        conta.deposito(valorTransferido)
+    }
+}
